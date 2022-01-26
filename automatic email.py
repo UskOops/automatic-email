62153f61c1e0715f8cdd41b84f4d224903bbba6a
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2022-01-26 13:08:20 -0300

    automatic email.py

---
+++ automatic email.py
@@ -0,0 +1,22 @@
+import smtplib 
+import email.message #library
+
+def send_email():
+    email_body = """
+    <p> Hello, this is a test email </p>
+    <p> Take care and wash your hands </p> """
+
+    msg = email.message.EmailMessage()
+    msg['Subject'] = 'Test Email'
+    msg['From'] = '#use your own email'
+    msg['To'] = '#enter destination email'
+    password = '#enter your password' #be careful, because your password will be exposed
+    msg.add_header('Content-Type', 'text/html')#add header to make sure the email is in html format
+    msg.set_payload(email_body)#set the email body
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)#connect to the server
+    s.starttls()#start the encryption
+    s.login(msg['From'], password)#login with your email and password
+    s.sendmail(msg['From'], msg['To'], msg.as_string())#send the email to the destination email 
+    print('Email sent')
+    s.quit()#close the connection
